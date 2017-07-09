b148140e8056d5353d83b3cfdad9859d09ec3b5f
Benjamin Friesen <bfriesen95@gmail.com>
2017-07-09 23:01:46 +0000

    pkg script update changes

--- .config/polybar/pkg.sh
+++ .config/polybar/pkg.sh
@@ -3,7 +3,8 @@ pac=$(checkupdates | wc -l)
 aur=$(cower -u | wc -l)
 
 check=$((pac + aur))
-if [[ "$check" != "0" ]]
-then
+if [[ "$check" != "0" ]]; then
     echo "$pac %{F#5b5b5b}%{F-} $aur"
+else
+    echo ""
 fi
